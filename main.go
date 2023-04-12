a09e6fc51d490ab01535e9c37a7bcdabec04eca7
Ernest <musaubrian15@gmail.com>
2023-04-12 10:50:46 +0300

    chore: leave program if can't open site files

--- main.go
+++ main.go
@@ -1,23 +1,55 @@
 package main
 
 import (
-	"github.com/fatih/color"
+	"bufio"
+	"log"
 	"net/http"
+	"os"
+
+	"github.com/fatih/color"
 )
 
+func readSites() ([]string, error) {
+	var sites []string
+	file, err := os.Open("./sites.txt")
+	if err != nil {
+        return sites, err
+	}
+	defer file.Close()
+
+	fScanner := bufio.NewScanner(file)
+	for fScanner.Scan() {
+		sites = append(sites, fScanner.Text())
+	}
+
+	return sites, nil
+}
+
+
 func main() {
-	sites_to_check := []string{"https://musaubrian.vercel.app", "https://openjournal.vercel.app", "https://github.com/musuabrian"}
-	err_msg := color.New(color.Bold, color.FgRed)
-	success := color.New(color.Bold, color.FgGreen)
+    err_msg := color.New(color.Bold, color.FgRed)
+    success := color.New(color.Bold, color.FgGreen)
+
+    sites_to_check, err := readSites()
+    if err != nil {
+        err_msg.Println("Could not open file", err)
+        os.Exit(1)
+    }
+
+    if len(sites_to_check) < 1 {
+        err_msg.Println("No sites found, try adding some")
+    }
 
 	for _, site := range sites_to_check {
 		resp, err := http.Get(site)
 		if err != nil {
-			err_msg.Printf("x %s may be down\n", err)
+			log.Fatal(err_msg.Println("x may be down", err))
 		}
+        if resp.StatusCode == 200 {
+            success.Printf("✓ (%d) %s\n", resp.StatusCode, site)
+        }
 		if resp.StatusCode != 200 {
-			err_msg.Printf("x (%d) %s not okay\n", resp.StatusCode, site)
+			err_msg.Printf("x (%d) %s\n", resp.StatusCode, site)
 		}
-		success.Printf("✓ (%d) %s\n", resp.StatusCode, site)
 	}
 }
