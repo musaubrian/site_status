ce98759f41a758b0f61117c33f4c0acbf5deed83
musaubrian <musaubrian15@gmail.com>
2023-05-31 09:36:39 +0300

    chore: remove url in sites.txt
    Show message with instructions

--- main.go
+++ main.go
@@ -34,40 +34,34 @@ func readSites(fileName string) ([]string, error) {
 
 func main() {
 	var (
-		filePath  string
-		err_msg   = color.New(color.Bold, color.FgRed)
-		success   = color.New(color.Bold, color.FgGreen)
-		just_bold = color.New(color.Bold)
+		filePath string
+		errMsg   = color.New(color.Bold, color.FgRed)
+		success  = color.New(color.Bold, color.FgGreen)
 	)
 
 	flag.StringVar(&filePath, "f", "./sites.txt", "The file location (full or relative path)\nExample: ~/Desktop/some_file.txt")
 	flag.Parse()
 
-	sites_to_check, err := readSites(filePath)
+	sitesToCheck, err := readSites(filePath)
 	if err != nil {
-		err_msg.Println("Could not open file", err)
+		errMsg.Println("Could not open file", err)
 		os.Exit(1)
 	}
 
-	if len(sites_to_check) < 1 {
-		err_msg.Println("No sites found, try adding some")
-	}
-	if len(sites_to_check) == 1 {
-        just_bold.Println("--------------")
-		just_bold.Print("we are using the default file list\nUse --help to see how to do this\n")
-        just_bold.Println("--------------")
+	if len(sitesToCheck) < 1 {
+		errMsg.Println("No sites found, try adding some\nUse --help to see how to do this")
 	}
 
-	for _, site := range sites_to_check {
+	for _, site := range sitesToCheck {
 		resp, err := http.Get(site)
 		if err != nil {
-			log.Fatal(err_msg.Println("x may be down", err))
+			log.Fatal(errMsg.Println("x may be down", err))
 		}
 		if resp.StatusCode == 200 {
 			success.Printf("✓ (%d) %s\n", resp.StatusCode, site)
 		}
 		if resp.StatusCode != 200 {
-			err_msg.Printf("x (%d) %s\n", resp.StatusCode, site)
+			errMsg.Printf("x (%d) %s\n", resp.StatusCode, site)
 		}
 	}
 }
